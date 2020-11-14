4a0fa27cd5cacb195eb261138775dbf71b65af61
BojidarPanaiotov <cccpisthebest@abv.bg>
2020-11-14 16:55:50 +0200

    Added GitHub requests

---
+++ Project/urlRepositories.js
@@ -0,0 +1,8 @@
+const baseUrl =`https://api.github.com/`;
+
+export const URLS = {
+    baseUrl: baseUrl,
+    firstUrl: `${baseUrl}` + `repos/KaloyanZulchev/Information/contents/Information/First.txt`,
+    secondUrl:  `${baseUrl}` + `repos/KaloyanZulchev/Information/contents/Information/Second.txt`,
+    thirdUrl:  `${baseUrl}` + `repos/KaloyanZulchev/Information/contents/Information/Third.txt`,
+}
