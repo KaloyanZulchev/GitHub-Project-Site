ef2076a17f020fb82f4d795ef5793ab65877bf75
KaloyanZulchev <64842320+KaloyanZulchev@users.noreply.github.com>
2020-11-16 00:54:27 +0200

    refactor: authentication file

--- Project/attachsEvents.js
+++ Project/attachsEvents.js
@@ -1,2 +1,3 @@
 import {elements} from "./elements.js"
 import tasksFunctions from "./taskManager.js"
+import {authenticationFunctions} from "./authentication.js"
--- Project/authentication.js
+++ Project/authentication.js
@@ -1,44 +1,27 @@
-import {elements} from "./elements.js"
-
+// import {elements} from "./elements.js"
+// elements.registrationButton().addEventListener('click', register);
+// elements.logInButton().addEventListener('click', userLogin);
+// elements.logOutButton().addEventListener('click', userLogout);
 const auth = firebase.auth();
 
-elements.registrationButton().addEventListener('click', register);
-elements.logInButton().addEventListener('click', userLogin);
-elements.logOutButton().addEventListener('click', userLogout);
+async function register(email, password) {
+  let response = await auth.createUserWithEmailAndPassword(email, password)
+    .then(response => {
+      const uid = response.user.uid;
+      const name = email.split('@')[0];
 
-function register(){
-    const email = elements.registrationEmail().value;
-    const password = elements.registrationPassword().value;
+      firebase.database()
+        .ref('personalInformation')
+        .child(uid)
+        .set({ name });
+    })
+    .catch();
 
-    auth.createUserWithEmailAndPassword(email, password)
-      .then(response => {
-        elements.registrationEmail().value = '';
-        elements.registrationPassword().value = '';
-
-        const uid = response.user.uid;
-        const name = email.split('@')[0];
-
-        firebase.database()
-          .ref('personalInformation')
-          .child(uid)
-          .set({name});
-      })
-      .catch(error => displayErrorMessage(error.message));
+  return response;
 }
-function displayErrorMessage(message){
-  elements.errorMessage().style.display = 'block';
-  elements.errorMessage().textContent = message;
 
-  setTimeout(() => {
-    elements.errorMessage().style.display = 'none';
-    elements.errorMessage().textContent = '';
-  }, 2500);
-}
-function userLogin(){
-  const email = elements.logInEmail().value;
-  const password = elements.logInPasswrd().value;
-
-  auth.signInWithEmailAndPassword(email, password)
+async function userLogin(email, password) {
+  let response = await auth.signInWithEmailAndPassword(email, password)
     .then(response => {
       if (response) {
         return response
@@ -46,12 +29,28 @@ function userLogin(){
 
       Promise.reject(response);
     })
-    .then(res => {
-      elements.logInEmail().value = '';
-      elements.logInPasswrd().value = '';
-    })
-    .catch(error => displayErrorMessage(error.message));
+    .catch();
+
+  return response;
 }
-function userLogout(){
-  auth.signOut();
+async function userLogout() {
+  let resoonse = await auth.signOut();
+
+  return resoonse;
 }
+
+export const authenticationFunctions = {
+  register,
+  userLogin,
+  userLogout
+};
+
+// function displayErrorMessage(message) {
+//   elements.errorMessage().style.display = 'block';
+//   elements.errorMessage().textContent = message;
+
+//   setTimeout(() => {
+//     elements.errorMessage().style.display = 'none';
+//     elements.errorMessage().textContent = '';
+//   }, 2500);
+// }
