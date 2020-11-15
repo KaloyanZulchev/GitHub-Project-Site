f4ff13c358609aba559843b28485474fc04a4db8
KaloyanZulchev <64842320+KaloyanZulchev@users.noreply.github.com>
2020-11-16 00:31:16 +0200

    refactor: task manager file

--- Project/taskManager.js
+++ Project/taskManager.js
@@ -1,65 +1,27 @@
-import {elements} from "./elements.js"
-
-const baseUrl = 'https://github---site.firebaseio.com/';
-
-elements.loadTasksButton().addEventListener('click', getTasks)
-elements.addTaskButton().addEventListener('click', addTask)
-//elements.updateTaskButton().addEventListener('click', updateTask)
-elements.finishTaskButton().addEventListener('click', () => {
-    
-
-    //to do
-})
-
-elements.deleteTaskButton().addEventListener('click', () => {
-   
-
-    //to do
-})
-
-function deleteTask(key){
+async function deleteTask(url){
     const uid = firebase.auth().currentUser.uid;
 
-    fetch(baseUrl + `personalInformation/${uid}/tasks/${key}.json`,{method: "DELETE"})
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    // url = baseUrl + `personalInformation/${uid}/tasks/${key}.json`
+
+    let response = await fetch(url, {method: "DELETE"});
+
+    return response;
 }
 
-function getTasks(){
+async function getTasks(url){
     const uid = checkForUser();
 
     if (uid == undefined) {
-        // To do send error message
-        return;
+        return undefined;
     }
 
-    fetch(baseUrl + `personalInformation/${uid}/tasks.json`)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
+    let response = await fetch(url);
 
-            Promise.reject(response);
-        })
-        .then(tasks => {
-            console.log(tasks); //to do
-        })
-        .catch(error => console.log(error)); // to do send error message
+    return response;
 }
 
-function addTask(){
+async function addTask(taskHeader, taskDescription, url){
     const uid = checkForUser();
-    const taskHeader = elements.taskHeader().value;
-    const taskDescription = elements.taskDescription().value;
-
-    if (uid == undefined || taskHeader == '' || taskDescription == '') {
-        // To do send error message
-        return;
-    }
     
     const taskObj = {
         header: taskHeader,
@@ -67,36 +29,22 @@ function addTask(){
         state: 'To Do'
     }
 
-    fetch(baseUrl + `personalInformation/${uid}/tasks.json`,
-        {method: "POST", body: JSON.stringify(taskObj)})
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
+    // url = baseUrl + `personalInformation/${uid}/tasks.json`
 
-            Promise.reject(response);
-        })
-        .then(key => {
-            elements.taskHeader().value = '';
-            elements.taskDescription().value = '';
-        })
-        .catch(error => {
-            console.log(error);// To do send error message
-        })
+    let response = await fetch(url, {method: "POST", body: JSON.stringify(taskObj)});
+    
+    return response;
 }
 
-function updateTaskState(key, state){
+async function updateTaskState(state, url){
     const uid = firebase.auth().currentUser.uid;
 
     const requestObj = {method: "PATCH", body: JSON.stringify({state})};
+    // url = baseUrl + `personalInformation/${uid}/tasks/${key}.json`
+    
+    let response = await fetch(url,requestObj)
 
-    fetch(baseUrl + `personalInformation/${uid}/tasks/${key}.json`,requestObj)
-        .then(response => {
-            //to do
-        })
-        .catch(error => {
-            // to do
-        });
+    return response;
 }
 
 function checkForUser(){
@@ -108,3 +56,12 @@ function checkForUser(){
 
     return user.uid;
 }
+
+const tasksFunctions = {
+    updateTaskState,
+    addTask,
+    deleteTask,
+    getTasks
+}
+
+export default tasksFunctions;
